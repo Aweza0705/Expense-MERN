95e18a678ee5a9301ef539a01740904cee9d6c98
Aweza Lareb <102857857+Aweza0705@users.noreply.github.com>
2024-07-22 23:07:56 +0530

    client added

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-light text-dark p-4">
+      <h6 className="text-center text-dark">
+        All rights reserved &copy; Team Coders
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
